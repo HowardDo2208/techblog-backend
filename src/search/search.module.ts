1fbfc7cd9a04a62623c7e0df1cb865e28c6922f2
Howard Do <howarddo2208@gmail.com>
2022-04-06 14:19:51 +0700

    add search module

---
+++ src/search/search.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common'
+import { ElasticsearchModule } from '@nestjs/elasticsearch'
+import { SearchService } from './search.service'
+
+@Module({
+  imports: [
+    ElasticsearchModule.registerAsync({
+      useFactory: () => ({
+        node: 'http://localhost:9200',
+      }),
+    }),
+  ],
+  providers: [SearchService],
+})
+export class SearchModule {}
