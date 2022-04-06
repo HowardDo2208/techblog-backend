25ede65239b55b257e1b3a3640b2330a99bac8bb
Howard Do <howarddo2208@gmail.com>
2022-04-06 14:20:42 +0700

    update lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { SearchModule } from './search/search.module'
+import { CatsController } from './cats/cats.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [SearchModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
