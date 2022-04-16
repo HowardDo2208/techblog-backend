36ab190305674f0d457dd64410bffedc686ab029
Howard Do <howarddo2208@gmail.com>
2022-04-16 12:32:34 +0700

    implement supertokens authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Session, UseGuards } from '@nestjs/common'
+import { SessionContainer } from 'supertokens-node/recipe/session'
 import { AppService } from './app.service'
+import { AuthGuard } from './auth/auth.guard'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('testAuth')
+  @UseGuards(AuthGuard)
+  testAuth(@Session() session: SessionContainer): string {
+    return JSON.stringify(session)
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { SearchModule } from './search/search.module'
 import { ConfigModule } from '@nestjs/config'
 import { DatabaseModule } from './database/database.module'
 import { PostsModule } from './posts/posts.module'
-import { UsersModule } from './users/users.module';
-import { PostCommentsModule } from './post-comments/post-comments.module';
-import { CategoryModule } from './category/category.module';
-import { TagModule } from './tag/tag.module';
+import { UsersModule } from './users/users.module'
+import { PostCommentsModule } from './post-comments/post-comments.module'
+import { CategoryModule } from './category/category.module'
+import { TagModule } from './tag/tag.module'
+import { AuthModule } from './auth/auth.module'
 
 const ENV = process.env.NODE_ENV
 
@@ -17,6 +18,20 @@ const ENV = process.env.NODE_ENV
     ConfigModule.forRoot({
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
     }),
+    AuthModule.forRoot({
+      // try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+      connectionURI:
+        'https://45b8e221bd3a11ecb9d523baaed2f04f-ap-southeast-1.aws.supertokens.io:3572',
+      apiKey: '0yNQ9HMNW5Rv8M9ZdyRuWZu6ZXpMH=',
+      appInfo: {
+        // Learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+        appName: 'techblog',
+        apiDomain: 'http://localhost:3000',
+        websiteDomain: 'http://localhost:3080',
+        apiBasePath: '/auth',
+        websiteBasePath: '/auth',
+      },
+    }),
     SearchModule,
     DatabaseModule,
     PostsModule,
@@ -24,6 +39,7 @@ const ENV = process.env.NODE_ENV
     PostCommentsModule,
     CategoryModule,
     TagModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
